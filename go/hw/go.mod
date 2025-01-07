04b75a7f76df7b5d32ec899b7f4ce1559e6e30e4
VexiDev <79677251+VexiDev@users.noreply.github.com>
2025-01-06 17:50:08 -0800

    created hello world and simple example test

--- go/hw/go.mod
+++ go/hw/go.mod
@@ -1,3 +1,3 @@
-module helloworld
+module vexi.gg/hello
 
 go 1.23.4
---
+++ go/hw/hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello()
+	expected := "Hello world!"
+
+	if got != expected {
+		t.Errorf("got %q expected %q", got, expected)
+	}
+}
